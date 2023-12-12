ba153c7a425eb3b306beb359a607c94a30fd1e4f
kostya_arsentev <bone_666@mail.ru>
2023-12-12 20:13:44 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+// DOM узлы
+const placesList = document.querySelector(".places__list");
+// Функция создания карточки
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  // DOM узлы
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  // колбэк удаления карточки
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => {
+      deleteCard(cardData);
+    });
+  cardData.element = cardElement;
 
-// @todo: DOM узлы
+  return cardElement;
+}
 
-// @todo: Функция создания карточки
+// Функция удаления карточки
+function deleteCard(cardData) {
+  cardData.element.remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  placesList.append(createCard(cardData, deleteCard));
+});
